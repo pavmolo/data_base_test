07343d9eecd07e756ccf99a605707827bb512b85
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-08-31 21:36:40 +0300

    Update kaizen_bd.py

--- kaizen_bd.py
+++ kaizen_bd.py
@@ -2,6 +2,7 @@ import streamlit as st
 import notion_df
 import pandas as pd
 
+notion_df.pandas()
 url_bd = st.secrets["page_url"]
 api_key = st.secrets["api_key"]
 df = pd.read_notion(url_bd, api_key=api_key)
